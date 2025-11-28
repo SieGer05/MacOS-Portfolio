2f92121e05dd2a783c17c0593810bca349024939
DJILI Mohamed Amine <aminedjili29@gmail.com>
2025-11-28 21:50:24 +0100

    Add real-time clock to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,25 @@
 import { navIcons, navLinks } from '#constants';
 import useWindowStore from '#store/window';
 import dayjs from 'dayjs';
+import { useState, useEffect } from 'react';
 
 const Navbar = () => {
    const { openWindow } = useWindowStore();
+   const [currentTime, setCurrentTime] = useState(dayjs());
+
+   useEffect(() => {
+      const timer = setInterval(() => {
+         setCurrentTime(dayjs());
+      }, 1000);
+
+      return () => clearInterval(timer);
+   }, []);
 
    return (
       <nav>
          <div>
-            <img src="/images/logo.svg" alt="logo" />
-            <p className='font-bold'>Djili's Portfolio</p>
+            <img src="/images/logo.svg" alt="logo" className="w-5" />
+            <p className='font-bold text-sm'>Djili's Portfolio</p>
 
             <ul>
                {navLinks.map(({ id, name, type }) => (
@@ -24,18 +34,20 @@ const Navbar = () => {
          </div>
 
          <div>
-            <ul>
+            <ul className="flex items-center gap-3 mr-4">
                {navIcons.map(({ id, img}) => (
                   <li key={id}>
-                     <img src={img} className='icon-hover' alt={`icon-${id}`} />
+                     <img src={img} className='icon-hover w-4 h-4' alt={`icon-${id}`} />
                   </li>
                ))}
             </ul>
 
-            <time >{dayjs().format('ddd MMM D h:mm A')}</time>
+            <time className="text-sm font-medium text-black min-w-[140px] text-right cursor-default select-none">
+               {currentTime.format('ddd MMM D h:mm A')}
+            </time>
          </div>
       </nav>
    );
 }
 
-export default Navbar
+export default Navbar;
